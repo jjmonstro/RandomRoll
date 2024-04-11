8bef5c04470ab703b1d31adb3a7e2054fbdc805c
Vitor <vitorsouzalibrelonrestini@gmail.com>
2024-04-11 12:44:13 -0300

    jj

---
+++ AtividadeROLL/AtividadeROLL/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+namespace AtividadeROLL
+{
+    public partial class MainPage : ContentPage
+    {
+   
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRoll_Clicked(object sender, EventArgs e)
+        {
+           double randomNumber = Convert.ToDouble(RandomNumber.Text);
+            double random = new Random().Next(0, 100);
+            RandomNumber.Text = randomNumber.ToString();
+    }
+    }
+
+}
